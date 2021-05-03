e16324a92d98443a5494a7a0c69110c36424d562
Claudio Heinrich <claudio@nr.no>
2021-05-03 18:27:27 +0200

    first version of Package...

---
+++ R/ncdf_to_dt.R
@@ -0,0 +1,107 @@
+
+#' function for converting netcdfs to long data tables.
+#' @param nc either character string with the name of the .nc file (including path), or an object of type ncdf4
+#' @param subset_list named list for subsetting. The names must match dimension names of the nc file. The range of the vector contained on each page defines which subset of the data is read out.
+#' @return long data table.
+#'
+#' @import ncdf4
+#'
+#' @examples {
+#' fn = '/nr/user/claudio/bigdisk/SFE/ERA_monthly_nc/total_precipitation_era_1979_1.nc'
+#' ncdf_to_dt(fn, subset_list = list('latitude' = -50:50)) # will read out everything between lat -50 and lat 50 (not only the latitudes matching -50:50)
+#' }
+#'
+#' @author Claudio
+#'
+#' @export
+
+ncdf_to_dt = function(nc,subset_list = NULL)
+{
+
+  if(is.character(nc)) nc = nc_open(nc)
+
+  dim_list = c()
+  dim_ids = c()
+  units = c()
+  dim_starts = c()
+  dim_counts = c()
+
+  #read dimension variables:
+  for(dim_ind in 1:nc$ndim)
+  {
+    nn = nc$dim[[dim_ind]]$name[1]
+
+    dd = ncvar_get(nc,nn)
+
+    if(nn %in% names(subset_list))
+    {
+      rel_indices = which(dd %between% range(subset_list[[which(names(subset_list) == nn)]]))
+
+    } else {
+      rel_indices = 1:length(dd)
+    }
+
+    dim_start = min(rel_indices)
+    dim_starts = c(dim_starts,dim_start)
+    names(dim_starts)[dim_ind] = nn
+
+    dim_count = max(rel_indices) - min(rel_indices) + 1
+    dim_counts = c(dim_counts,dim_count)
+
+    dd = dd[dim_start:(dim_start + dim_count - 1)]     # NOT dd[rel_indices], since dd is not neccessarily sorted?
+
+
+    dim_list = c(dim_list,list(dd))
+    names(dim_list)[dim_ind] = nn
+    dim_ids = c(dim_ids,nc$dim[[dim_ind]]$id)
+
+    units = c(units,paste0(nc$dim[[dim_ind]]$name[1],':   ',nc$dim[[dim_ind]]$units))
+  }
+
+  ### sometimes the dimensions follow a different order in the variables as in nc$dim??? ###
+
+  dim_order = nc$var[[1]]$dimids
+
+  # check whether all variables have the same dimesnion ordering:
+  if(nc$nvars > 1)
+  {
+    for(var_id in 2:nc$nvars)
+    {
+      if(!identical(dim_order,nc$var[[var_id]]$dimids))
+      {
+        stop('different variables have different dimension ordering, currently not supported.')
+      }
+    }
+  }
+
+  dim_list = dim_list[match(dim_ids,dim_order)]
+  dim_starts = dim_starts[match(dim_ids,dim_order)]
+  dim_counts = dim_counts[match(dim_ids,dim_order)]
+
+  dim_dt = as.data.table(expand.grid(dim_list))
+
+
+
+  # append variable values to dim_dt:
+
+  return_dt = dim_dt
+
+
+  for(var_ind in 1:nc$nvars)
+  {
+
+    temp = ncvar_get(nc, varid = nc$var[[var_ind]]$name,start = unname(dim_starts),count = unname(dim_counts))
+
+    var_dt = data.table( as.vector(temp))
+    setnames(var_dt,nc$var[[var_ind]]$name)
+
+    return_dt = data.table(return_dt,var_dt)
+    units = c(units,paste0(nc$var[[var_ind]]$name,':   ',nc$var[[var_ind]]$units))
+  }
+
+  # printout units:
+  catout = paste0(c('Units:',units),sep = '',collapse = "\n")
+  cat(catout)
+
+  return(return_dt)
+}
---
+++ R/plotting.R
@@ -0,0 +1,479 @@
+
+
+#' plotting function for precip fields
+#'
+#' @description plots dat on a regular grid over a map.
+#'
+#' @param dt Data table containing columns lon, lat and the data. The data defaults to the third column. If multiple time-steps are available in dt, only the the data to the first one is plotted.
+#' @param data_col The name of the column in dt containing the data.
+#' @param rr range of the plotted variable, default is the range of the data. Manipulate this if you compare multiple plots.
+#' @param mn plot title
+#' @param legend_title legend title.
+#' @param colorscale A ScaleContinuous object specifying the scale for the plot layer fill. Can be used to change colors or legend title (by setting name).
+#' @param mapscale Either 'low', 'medium' or 'high'. Controls the scale of the map. Higher scale leads to better resolved
+#'
+#' @return a ggplot object.
+#'
+#' @import data.table
+#' @import ggplot2
+#' @importFrom rnaturalearth ne_countries
+#'
+#' @export
+#'
+#' @author Claudio Heinrich
+
+
+ggplot_dt = function(dt,
+                     data_col = colnames(dt)[3],
+                     rr = NULL,
+                     mn = NULL,
+                     colorscale = scale_fill_gradient2(low = "blue", mid = "white", high = "red",name = data_col),
+                     mapscale = 'large')
+{
+  ####### transform data #######
+
+  # if you have spatio-temporal data, plot only only the first time-slice of it (convenient for diagnostics).
+
+  time_cols = intersect(names(dt),c('month','year','day','date','season'))
+
+  dt_sm = dt[,.SD,.SDcols = c('lon','lat',data_col,time_cols)][!is.na(get(data_col))]
+
+  if(length(time_cols)>0)
+  {
+    tc1 = dt_sm[1,.SD,.SDcols = time_cols]
+    dt_sm = merge(dt_sm,tc1,by = time_cols)
+  }
+
+  #### get map: ####
+
+  world_map <- rnaturalearth::ne_countries(scale = mapscale, returnclass = "sf")
+
+  #### fix range and set values outside of range to the range border ####
+
+  if(is.null(rr))
+  {
+    rr = dt_sm[,range(get(data_col))]
+  }
+
+  lower_indices = dt_sm[,which(get(data_col) < rr[1])]
+  upper_indices = dt_sm[,which(get(data_col) > rr[2])]
+
+  dt_sm[lower_indices,(data_col) := rr[1]]
+  dt_sm[upper_indices,(data_col) := rr[2]]
+
+  ### setup for plotting ###
+
+  lats = sort(unique(dt_sm[,lat]))
+  lons = sort(unique(dt_sm[,lon]))
+
+  dt_sm = dt_sm[!is.na(get(data_col))]
+
+  lon_dist = lons[2] - lons[1]
+  lat_dist = lats[2] - lats[1]
+
+  ### plotting ###
+  pp = ggplot(data = dt_sm) +
+    geom_rect(xmin = min(lons) - lon_dist/2,                           # add gray rectangle in the background, such that missing values appear gray
+              xmax = max(lons) + lon_dist/2,
+              ymin = min(lats) - lat_dist/2,
+              ymax = max(lats) + lat_dist/2,fill = 'gray') +
+    geom_tile(aes(x = lon,y = lat, fill = get(data_col))) +            # add data plot
+    geom_sf(data = world_map,alpha = 0)  +                             # add map
+    colorscale +                                                       # colorscale is specified in the function head
+    coord_sf(xlim = range(lons),ylim = range(lats),expand = FALSE) +   # restricts the plot to exactly the considered area to avoid weird borders
+    xlab('') + ylab('') +                                              # remove default labels and background grid...
+    theme(panel.background = element_blank(),
+          axis.text = element_blank(),
+          axis.ticks = element_blank())
+
+  if(!is.null(mn)) pp = pp + ggtitle(mn)                               # add title, if given
+
+  return(pp)
+}
+
+
+
+
+#' Turn a matrix with lat/lon idices into a data.table
+#' @export
+mat_to_dt = function(X, lons, lats, var_name = "X"){
+  n_lat = length(lats)
+  n_lon = length(lons)
+  dt = data.table(lon = rep(lons, times = length(lats)),
+                  lat = rep(lats, each = length(lons)),
+                  X = as.vector(X))
+  names(dt)[3] = var_name
+  return(dt)
+}
+
+
+
+#' Quick diagnostic plotting function
+#'
+#' @description Takes a data table that contains cols Lon, Lat (or lon and lat) and a variable to plot.
+#'
+#' @param dt The data table.
+#' @param var Character string. The name of the column containing the values for the plot. Default is third column, for subset data tables of the form .(Lon,Lat,var).
+#' @param mn Title of the plot.
+#' @param col_scheme Either "bwr" for blue - white - red, "wr" for white - red, or "wb" for white - blue. Specifies the color scheme of the plot.
+#' @param set_white Forces the blue-white-red color scheme to center white at the set value if specified.
+#' @param xlab,ylab Labeling.
+#' @param save_pdf,save_dir,file_name Whether, where and under which name the plot should be saved.
+#' @param stretch_par Numeric. Only used when save_pdf == TRUE. Stretches the pdf output. Default is NULL, where it is stretched to #Lons/#Lats.
+#' @param legend Boolean. if FALSE, no legend is provided
+#'
+#' @return none
+#'
+#' @export
+#'
+#' @author Claudio Heinrich
+#' @examples \dontrun{
+#' dt = load_combined_wide()
+#' plot_diagnostic(dt)
+#' }
+#'
+#' @importFrom fields designer.colors image.plot
+#' @importFrom maps map
+plot_diagnostic = function( dt, var = colnames(dt)[3], mn = var,
+                            rr = NULL,
+                            col_scheme = "bwr", set_white = NULL,
+                            xlab = "", ylab = "",
+                            save_pdf = FALSE, save_dir = "./figures/", file_name = "diag_plot",stretch_par = NULL,
+                            legend = TRUE)
+{
+  # sometimes lat and lon are given with lowercase letters:
+
+  if('lat' %in% colnames(dt))
+  {
+    dt[,Lat:=lat][,Lon:=lon]
+  }
+
+  # prepare data table
+
+  if("year" %in% colnames(dt))
+  {
+    if("month" %in% colnames(dt))
+    {
+      dt = dt[year == min(year)][month == min(month),.SD,.SDcols = c('Lon','Lat',var)][order(Lat,Lon)]
+    } else {
+      dt = dt[year == min(year),.SD,.SDcols = c('Lon','Lat',var)][order(Lat,Lon)]
+    }
+  } else {
+    dt = dt[,.SD,.SDcols = c('Lon','Lat',var)][order(Lat,Lon)]
+  }
+
+
+  dt[Lon > 180, Lon := Lon-360]
+
+
+  #--- get longitudes and latitudes
+
+  Lons = sort(unique(dt[,Lon]))
+  Lats = sort(unique(dt[,Lat]))
+
+  n_lon = length(Lons)
+  n_lat = length(Lats)
+
+  # in case the grid is incomplete:
+
+  if(n_lon * n_lat > dt[,.N])
+  {
+    setkey(dt,Lat,Lon) # doesn't change ordering
+
+    full_coords = data.table(Lat = rep(Lats,each = n_lon) , Lon = rep(Lons,n_lat))
+    setkey(full_coords,Lat,Lon)
+
+    dt = merge(dt,full_coords,all.y = TRUE)
+  }
+
+  if(is.null(rr))  rr = range(dt[,3],na.rm=TRUE)
+  if(!is.null(rr)){
+    dt[dt[[3]]<min(rr),3] = min(rr)
+    dt[dt[[3]]> max(rr),3] = max(rr)
+  }
+
+  A = matrix(dt[[3]],  n_lon, n_lat)
+
+  # --- scaling and colors ---
+
+  brk = seq(rr[1],rr[2],length = 500)
+  brk.ind = round(seq(1,length(brk),length = 10))
+  brk.lab = round(brk[brk.ind],2)
+  brk.at = brk[brk.ind]
+
+  if(col_scheme == "bwr"){
+    if(is.null(set_white)){
+      color <- fields::designer.colors(n=length(brk)-1, col = c("darkblue","white","darkred"))
+    }else{
+      zero.ind = min(which(brk > set_white))/length(brk)
+      color <- fields::designer.colors(n=length(brk)-1, col = c("darkblue","white","darkred"), x = c(0,zero.ind,1))
+    }
+  }
+  if(col_scheme == "wr"){
+    color <- fields::designer.colors(n=length(brk)-1, col = c("white","darkred"))
+  }
+  if(col_scheme == "wb"){
+    color <- fields::designer.colors(n=length(brk)-1, col = c("white","blue"))
+  }
+
+  # set up for coloring NAs gray
+
+  newz.na <- rr[2]+(rr[2]-rr[1])/length(color) # new z for NA
+  A[which(is.na(A))] <- newz.na
+  rr[2] <- newz.na      #extend the range to include the new value
+  color <- c(color, 'gray') # extend the color palette by 'gray'
+  brk = c(brk,rr[2]) # extend the vector of breaks
+
+  #--- plotting ---
+
+  if(save_pdf)
+  {
+    if (is.null(stretch_par)) stretch_par = n_lat/n_lon
+
+    par_0 = par() # allow to set par manually before calling the function
+
+    pdf(paste0(save_dir,file_name,".pdf"),width = 7,height = stretch_par * 7)
+
+    suppressWarnings(par(par_0))
+  }
+
+  if(legend)
+  {
+
+    fields::image.plot(Lons,Lats,A,
+                       zlim=rr, main = mn,
+                       xlim = range(Lons), xlab=xlab,
+                       ylim = range(Lats), ylab=ylab,
+                       breaks=brk,
+                       col=color,
+                       axis.args=list(at = brk.at,
+                                      label = brk.lab))
+  } else {
+    image(Lons,Lats,A,
+          zlim=rr, main = mn,
+          xlim = range(Lons), xlab=xlab,
+          ylim = range(Lats), ylab=ylab,
+          breaks=brk,
+          col=color)
+
+  }
+
+
+  # add world map
+  maps::map("world", add = TRUE)
+
+  if(save_pdf) dev.off()
+
+}
+
+#' Take a data.table on an incomplete grid and "squarify" so that the image function can work on it
+#' @param dt A data.table with Lon and Lat plus another variable
+#' @param var The name of the variable to be squarified
+#' @export
+squarify = function(dt, var = names(dt)[3])
+{
+  lons = dt[,unique(Lon)]
+  lats = dt[,unique(Lat)]
+
+  DT_c = data.table(Lon = rep(lons,each = length(lats)), Lat  = rep(lats, times = length(lons)))
+  DT_plot = merge(DT_c,
+                  dt[,.SD,.SDcols = c("Lon","Lat",var)],
+                  by = c("Lon","Lat"),
+                  all.x = TRUE,
+                  all.y = FALSE)
+
+  return(DT_plot)
+
+
+}
+
+
+
+
+#' smooth plotting function
+#'
+#' @description Takes a data table of the form .(Lon,Lat,value) or .(Lat,Lon,value) and plots values on the globe after applying a kernel smoothing
+#'
+#' @param dt The data table.
+#' @param var Character string. The name of the column containing the values for the plot. Default is the name of the third column of dt, to be used for subset data tables of the form .(Lon,Lat,var).
+#' @param mn,rr Title and range of the plot.
+#' @param theta parameter for the Gaussian smoothing kernel
+#' @param pixels Resolution of the plot
+#' @param col_scheme Either "bwr" for blue - white - red, "wr" for white - red, or "wb" for white - blue. Specifies the color scheme of the plot.
+#' @param set_white Forces the blue-white-red color scheme to center white at the set value if specified.
+#' @param xlab,ylab Labeling.
+#' @param save_pdf,save_dir,file_name Whether, where and under which name the plot should be saved.
+#' @param stretch_par Numeric. Only used when save_pdf == TRUE. Stretches the pdf output. Default is NULL, where it is stretched to #Lons/#Lats.
+#'@param exclude_land Should pixels on land be grayed out?
+#' @param exclude_ocean Should pixels on ocean be grayed out?
+#' @param legend Should the plot have a legend?
+#' @return none
+#'
+#' @export
+#'
+#' @author Claudio Heinrich
+#' @examples \dontrun{
+#' dt = load_combined_wide()
+#' plot_smooth(dt)
+#' }
+#'
+#' @importFrom fields as.image designer.colors image.plot image.smooth
+#' @importFrom maps map
+#' @import maptools
+#' @importFrom sp CRS over proj4string SpatialPoints
+
+
+plot_smooth = function(dt_plot,
+                       var = colnames(dt)[3],
+                       mn = var,
+                       rr = NULL,
+                       theta = 0.5,
+                       pixels = 256,
+                       col_scheme = "bwr",
+                       set_white = NULL,
+                       xlab = "Lon",
+                       ylab = "Lat",
+                       save_pdf = FALSE,
+                       save_dir = "./figures/",
+                       file_name = "diag_plot",
+                       stretch_par = NULL,
+                       exclude_land = FALSE,
+                       exclude_ocean = FALSE,
+                       legend = TRUE)
+{ # sometimes lat and lon are given with lowercase letters:
+
+  dt = copy(dt_plot)
+
+  if('lat' %in% colnames(dt)){
+    dt[,Lat:=lat][,Lon:=lon]
+  }
+  if(any(dt[, Lon] > 180)){
+    dt[Lon > 180, Lon := Lon - 360]
+  }
+
+
+  dt[Lon > 180, Lon := Lon-360]
+
+  ## prepare data table
+  if("year" %in% colnames(dt)){
+    if("month" %in% colnames(dt)){
+      dt = dt[year == min(year)][month == min(month),.SD,.SDcols = c('Lon','Lat',var)][order(Lat,Lon)]
+    }else{
+      dt = dt[year == min(year),.SD,.SDcols = c('Lon','Lat',var)][order(Lat,Lon)]
+    }
+  }else{
+    dt = dt[,.SD,.SDcols = c('Lon','Lat',var)][order(Lat,Lon)]
+  }
+
+  dt = squarify(dt)
+  ##--- create image ---
+
+
+  x = dt[,.(Lon,Lat)]
+  setnames(x,c("Lon","Lat"), c("lon","lat"))
+
+  Lons = unique(dt[,Lon])
+  Lats = unique(dt[,Lat])
+
+  n_lon = length(Lons)
+  n_lat = length(Lats)
+
+  A = matrix(dt[[3]],  n_lon, n_lat)
+
+  im_0 = fields::image.smooth(fields::as.image(A,x = x,nx = pixels,ny = pixels),theta = theta)
+
+  ## Find the points that fall over land
+
+  if(!exists("wrld_simpl")) data(wrld_simpl, package = 'maptools')
+
+  all_loc = expand.grid(lat = im_0$x,lon = im_0$y)
+  pts <- sp::SpatialPoints(all_loc, proj4string=sp::CRS(sp::proj4string(wrld_simpl)))
+  ii <- !is.na(sp::over(pts, wrld_simpl)$FIPS)
+  if(exclude_land){
+    im_0$z[ii] = NA
+  }
+  if(exclude_ocean){
+    im_0$z[-which(ii)] = NA
+  }
+
+  ## --- fix range of plot and fill in values for points out of range ---
+  if(is.null(rr)){
+    rr = range(im_0$z,na.rm=TRUE)
+  }else{
+    im_0$z[im_0$z< min(rr)] = min(rr)
+    im_0$z[im_0$z> max(rr)] = max(rr)
+  }
+
+  ## --- Get the symmetric color scaling ---
+  brk = seq(rr[1],rr[2],length = 500)
+  if(all(brk < 0)){
+    ## If everthing is negative
+    cr = colorRamp(c("white", "darkblue"))
+    bmin = min(brk) ## Find the coldest
+    brk_fraction = brk / bmin
+    brk_col = rgb(cr(brk_fraction), max = 255)
+  }
+  if(all(brk > 0)){
+    ## If everthing is postive
+    cr = colorRamp(c("white", "darkred"))
+    bmax = max(brk) ## Find the warmest
+    brk_fraction = brk / bmax
+    brk_col = rgb(cr(brk_fraction), max = 255)
+  }
+  if(any(brk < 0) & any(brk > 0)){
+    ## If we span 0, then make sure the two ends are equally shaded in magnitude
+    r_max = max(abs(rr))
+    cr_neg = colorRamp(c("white", "darkblue"))
+    cr_pos = colorRamp(c("white", "darkred"))
+    brk_col = rep(NA, length(brk))
+    w_neg = which(brk < 0)
+    brk_col[w_neg] = rgb(cr_neg(brk[w_neg] / -r_max), max = 255)
+    brk_col[-w_neg] = rgb(cr_pos(brk[-w_neg] / r_max), max = 255)
+  }
+  brk_ind = round(seq(1,length(brk),length = 10))
+  brk_lab = round(brk[brk_ind],2)
+  brk_at = brk[brk_ind]
+  ##--------------------------------------------------------------------------
+
+  ## color NAs grey
+  newz.na <- rr[2]+(rr[2]-rr[1])/length(brk_col) # new z for NA
+
+  im_0$z[which(is.na(im_0$z))] <- newz.na
+  rr[2] <- newz.na # extend the range to include the new value
+  brk_col <- c(brk_col, grey(.7))
+  brk = c(brk,rr[2]) # extend the vector of breaks
+
+  ##--- plotting ---
+  if(save_pdf){
+    if (is.null(stretch_par)){
+      stretch_par = n_lat/n_lon
+    }
+    pdf(paste0(save_dir,file_name,".pdf"),width = 7,height = stretch_par * 7)
+  }
+
+  if(legend){
+    if(!exists('axis.args')){
+      axis.args=list(at = brk_at,
+                     label = brk_lab)
+    }
+    fields::image.plot(im_0,
+                       zlim=rr, main = mn,
+                       xlim = range(Lons),
+                       xlab = xlab,
+                       ylim = range(Lats),
+                       ylab = ylab,
+                       breaks=brk,
+                       col=brk_col[-1])
+  }else{
+    image(im_0,
+          zlim=rr, main = mn,
+          xlim = range(Lons), xlab=xlab,
+          ylim = range(Lats), ylab=ylab,
+          breaks=brk,
+          col=brk_col[-1])
+  }
+  ## add world map
+  maps::map("world", add = TRUE)
+  if(save_pdf) dev.off()
+
+}
